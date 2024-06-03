cbdb5c4513fb083263dc7e2daa98735a73e60dcb
BhoomiShakya <siashakya509@gmail.com>
2024-06-03 15:02:47 +0530

    jhsjk

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ connectDB();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000'] }));
 
 app.use('/user', AuthRoute);
 app.use('/playlist', PlayList);
