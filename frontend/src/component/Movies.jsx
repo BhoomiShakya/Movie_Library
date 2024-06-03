50b6741735f0bb786ecc2a230e874ecf9e3fc1c8
BhoomiShakya <siashakya509@gmail.com>
2024-06-03 20:27:53 +0530

    ghj

--- frontend/src/component/Movies.jsx
+++ frontend/src/component/Movies.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import '../css/Movies.css';
 import Navbar from './Navbar';
-import Dialog from './Dialog'; 
+// import { Link } from 'react-router-dom';
+import Dialog from './Dialog'; // Create this component
 
-//shoowing movies fetched from API
 function Movies({ movies , setUserIsLoggedIn}) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [selectedMovie, setSelectedMovie] = useState(null);
@@ -22,14 +22,14 @@ function Movies({ movies , setUserIsLoggedIn}) {
     return (
         <div>
             <Navbar setUserIsLoggedIn={setUserIsLoggedIn}/>
-            <div className='heroo_section'>
-                <div className="movie_result_list">
+            <div className='heroo'>
+                <div className="movie-list">
                     {movies.map((movie) => (
-                        <div key={movie.imdbID} className="details_list">
+                        <div key={movie.imdbID} className="movie-details">
                             <h2>{movie.Title}</h2>
                             <p><strong>Year:</strong> {movie.Year}</p>
                             <p><strong>Type:</strong> {movie.Type}</p>
-                            <div className='poster_container'>
+                            <div className='posterdiv'>
                                 {movie.Poster !== "N/A" && <img src={movie.Poster} alt={movie.Title} />}
                             </div>
                             <button className="add-button" onClick={() => handleAddClick(movie)}>+</button>
