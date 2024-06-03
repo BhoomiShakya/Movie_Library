5216d520e93086e59e437f7ff40372916b7c6c2d
BhoomiShakya <siashakya509@gmail.com>
2024-06-03 14:34:11 +0530

    main_jhfhg

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from '../images/sia_logo.png';
 import '../css/Navbar.css';
+import {Link} from 'react-router-dom';
 
 const Navbar = ({ setUserIsLoggedIn }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -55,13 +56,13 @@ const Navbar = ({ setUserIsLoggedIn }) => {
             </div>
             <div className="navbar-links">
                 {!isLoggedIn ? (
-                    <>
-                        <a href="/login">Login</a>
-                        <a href="/signup">Signup</a>
-                    </>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
                 ) : (
                     <div className='side-icons'>
-                        <a href="/" style = {{paddingRight : "1.5rem"}}>{username}</a>
+                        <Link to="/" style = {{paddingRight : "1.5rem"}}>{username}</Link>
                         <button onClick={handleLogout} className='logOutbtn'>Logout</button>
                     </div>
                 )}
